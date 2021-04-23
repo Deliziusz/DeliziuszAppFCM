9ecd6388b6e9fe62e6f3ef0070d4311df06f75b6
Deliziusz <deliziusz.666@gmail.com>
2021-04-22 23:40:44 -0500

    Agregamos usuarios conectados por Firebase yeah yeah yeah.... Inicio de sesión, clarooo

---
+++ app/src/main/java/zombie/deliziusz/deliziuszappfcm/InicioSesionExitoso.java
@@ -0,0 +1,14 @@
+package zombie.deliziusz.deliziuszappfcm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InicioSesionExitoso extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_sesion_exitoso);
+    }
+}
